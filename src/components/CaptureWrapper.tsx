4b2b80f0d70cec30348823a3ecfa3058a8b38db1
Rachel Scott <rachelsgcount@gmail.com>
2024-01-27 18:09:32 -0500

    Adding text stuff

--- src/components/CaptureWrapper.tsx
+++ src/components/CaptureWrapper.tsx
@@ -7,18 +7,57 @@
 
 import React, {createContext, Dispatch, SetStateAction, useEffect, useState} from "react"
 import {Canvas} from "@react-three/fiber"
-import * as fs from 'fs'
 
 type GameState = [ 'pregame' | 'story' | 'game' | 'end', Dispatch<SetStateAction<'pregame' | 'story' | 'game' | 'end'>>]
 export const GameContext = createContext<GameState>(null!)
 
 //Global variables
-export const chosenWords: string[][] = []
-export const nouns: string[] = (fs.readFileSync('../text/nouns.txt', 'utf-8')).split('\r\n')
-export const adjectives: string[] = (fs.readFileSync('../text/adjectives.txt', 'utf-8')).split('\r\n')
-export const verbs: string[] = (fs.readFileSync('../text/verbs.txt', 'utf-8')).split('\r\n')
-export const adverbs: string[] = (fs.readFileSync('../text/adverbs.txt', 'utf-8')).split('\r\n')
+export const chosenWords: string[10][] = []
+// verb
+// 1) adj, noun, present tense verb
+// 2) verb end in s, verb ending in ing, noun
+// 3) adj, noun, past verb, past verb, past verb
+// 4) #3 noun, noun
+// 5) verb, adj, plural noun
+// 6) present tense verb, adverb, present tense verb, adverb
+// 7) relationship noun, adj, noun
+// 8) verb, #3 noun, adj
+// 9) adj, noun, #2 noun, present tense verb
 
+export const script: string[] = [
+    //tutorial
+    "Murphy has insomnia, so like many other nights, he finds himself ________ Waffle House at 2 am.",
+    //1
+    "He ________  ________  ________ in the early morning.",
+    //2
+    "Taking a seat next to a ----- (adj) ----- (noun), he orders his hashbrowns -----, -----, and ----- (past tense verbs).",
+    //3
+    "The XXX (previous noun) orders a ----- (noun.",
+    //4
+    "When the meal arrives, he exclaims: \"I ----- (verb) ----- (adj) ----- (plural noun) !\" and eats ----- (adverb).",
+    //5
+    "Murphy's stomach ----- (present tense verb) ----- (adverb), which makes Murphy ----- (present tense verb) ----- (adverb).",
+    //6
+    "But then suddenly, and without warning, Murphy's ----- (relationship noun), the ----- (adj) ----- (noun), sat down across to him.",
+    //7
+    "They started to ----- (verb), and the XXX (previous noun) left, ----- (adj).",
+    //8
+    "After this, Murphy decides it's time to head home. He bids everyone a ----- (adj) ----- (noun), boards his XXX (previous noun), and ----- (present tense verb) home.",
+    //9
+    "",
+]
+export const nouns: string[] = [
+
+]
+export const adjectives: string[] = [
+
+]
+export const verbs: string[] = [
+
+]
+export const adverbs: string[] = [
+
+]
 
 export default function CaptureWrapper({ children }: { children: React.ReactNode }) {
     const canvasRef = React.useRef<HTMLCanvasElement>(null!)
