ecf7a1569d23d605d0b47f6126d7b9396db2db3c
Rachel Scott <rachelsgcount@gmail.com>
2024-01-28 16:12:45 -0500

    Update StoryResult.tsx

--- src/components/StoryResult.tsx
+++ src/components/StoryResult.tsx
@@ -1,6 +1,5 @@
-import { useContext, useState } from "react"
 import { Text, Float } from "@react-three/drei"
-import { GameContext, script } from "./CaptureWrapper"
+import { script } from "./CaptureWrapper"
 import { chosenWords } from "./CaptureWrapper"
 
 let scriptIndex: number
@@ -8,13 +7,14 @@ let combinedScript: string
 
 
 export default function StoryResult( ) {
-    const [ gameState, setGameState ] = useContext(GameContext)
     scriptIndex = 0
-    combinedScript = ""
     textAppear(0)
 
     const handleClick = () => {
         scriptIndex++
+        if(scriptIndex == script.length){
+            scriptIndex = 0
+        }
         textAppear(scriptIndex)
     }
 
@@ -43,11 +43,12 @@ export default function StoryResult( ) {
 function textAppear(num: number) {
     let chosenWordIndex: number = 0
     let scripttt: string[] = script[0].split("_____")
-    
+    combinedScript = ""
+
     for (let index = 0; index < scripttt.length - 1; index++) {
         const element = scripttt[index];
         combinedScript += (element)
-        combinedScript += (chosenWords[num][chosenWordIndex].n)
+        combinedScript += (chosenWords[num][chosenWordIndex])
         chosenWordIndex++
     }
     combinedScript += (scripttt[scripttt.length -1])
@@ -57,38 +58,51 @@ function textAppear(num: number) {
 }
 
 function nounAppear(num: number) {
+    let nouns: string[] = new Array<string>()
+
     switch (num) {
-        case 0:
-            
+        case 0: // 0) verb
             break;
-        case 1:
-            
+        case 1: // 1) adj, noun, present tense verb
+            nouns.push("MurphyRide.tsx")
+            nouns.push(chosenWords[num][1] + ".tsx")
             break;
-        case 2:
-
+        case 2: // 2) verb end in s, verb ending in ing, noun
+            nouns.push(chosenWords[num][2] + ".tsx")
             break;
-        case 3: 
-            
+        case 3: // 3) adj, noun, past verb, past verb, past verb
+            nouns.push(chosenWords[num][1] + ".tsx")
             break;
-        case 4:
-
+        case 4: // 4) #3 noun, noun
+            nouns.push(chosenWords[num][0] + ".tsx")
+            nouns.push(chosenWords[num][1] + ".tsx")
             break;
-        case 5:
-
+        case 5: // 5) verb, adj, plural noun
+            nouns.push(chosenWords[num][2] + ".tsx")
+            nouns.push(chosenWords[num][2] + ".tsx")
+            nouns.push(chosenWords[num][2] + ".tsx")
             break;
-        case 6:
-
+        case 6: // 6) present tense verb, adverb, present tense verb, adverb
             break;
-        case 7:
-
+        case 7: // 7) relationship noun, adj, noun
+            nouns.push(chosenWords[num][2] + ".tsx")
             break;
-        case 8:
-
+        case 8: // 8) verb, #3 noun, adj
+            nouns.push(chosenWords[num][1] + ".tsx")
             break;
-        case 9:
-
+        case 9: // 9) adj, noun, #2 noun, present tense verb
+            nouns.push(chosenWords[num][1] + ".tsx")
+            nouns.push(chosenWords[num][2] + ".tsx")
             break;
         default:
             break;
     }
+    if(num != 1){
+        nouns.push("Murphy.tsx")
+    }
+
+    console.log(nouns)
+    //nouns.forEach(element => {
+        
+    //});
 }
